3e813c808b2e67cf512f44298112ebb3b10b1085
Daniel Lauding <daniellauding@daniellaudingse.local>
2017-01-09 08:02:32 -0600

    testing headroom

--- public/index.php
+++ public/index.php
@@ -36,6 +36,36 @@
 
         <?php include("partials/latest-news.php");?>
 
+        <div class="widget conferences">
+
+          <div id="conferences-carousel" class="carousel slide" data-ride="carousel">
+
+            <div class="carousel-inner">
+
+              <div class="carousel-item active">
+                <p>24 jan 2017</p>
+                <h3><a href="">Hur driver och säkerställer vi ett gott mottagande av nyanlända elever i skola och kommun?</a></h3>
+                <button type="button" class="btn btn-link">Läs mer</button>
+              </div>
+
+              <div class="carousel-item">
+                <p>24 jan 2017</p>
+                <h3><a href="">Lorem ipsum dolor sit amet, duo eius adolescens at, meis dicat facer te vix. Ea utroque partiendo vix.</a></h3>
+                <button type="button" class="btn btn-link">Läs mer</button>
+              </div>
+
+            </div>
+
+            <ol class="carousel-indicators">
+              <li data-target="#conferences-carousel" data-slide-to="0" class="active"></li>
+              <li data-target="#conferences-carousel" data-slide-to="1" class=""></li>
+              <!-- <li data-target="#myCarousel" data-slide-to="2" class=""></li> -->
+            </ol>
+
+          </div>
+
+        </div>
+
         <div class="banner-area">
           <h6 class="banner-title">ANNONS:</h6>
           <div class="banner-content">
@@ -62,6 +92,8 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
   <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
   <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.headroom.js"></script>
+  <script src="js/headroom.min.js"></script>
   <script src="js/scripts.js"></script>
 </body>
 </html>
