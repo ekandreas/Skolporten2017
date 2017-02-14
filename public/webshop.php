61b180005b1fb8f00442ebbf8c73089f29069751
Daniel Lauding <daniellauding@daniellaudingse.local>
2017-02-14 16:13:10 -0500

    webshop

--- public/webshop.php
+++ public/webshop.php
@@ -24,88 +24,88 @@
             <div class="meta">
               <!-- <a href="#" class="meta-link" target="_blank">2016-12-19 11:09</a> -->
               <a href="#" class="meta-link" target="_blank"><img src="css/gfx/icon-share.png"/> Dela</a>
-              <a href="#" class="meta-link" target="_blank"><img src="css/gfx/icon-print.png"/> Skriv ut</a>
+              <!-- <a href="#" class="meta-link" target="_blank"><img src="css/gfx/icon-print.png"/> Skriv ut</a> -->
             </div>
           </div>
 
-          <div class="col-md-3">
+          <div class="col-xs-12 col-md-6 col-lg-3">
             <div class="well shop-item">
               <img src="../images/skolporten-poster.png"/>
               <h5>Skolporten nr 1/2017</h5>
               <p>Pris: 169 Kr inklusive moms</p>
               <div class="form-group row">
-                <label for="" class="col-2 col-form-label">Antal</label>
-                <div class="col-10">
+                <label for="" class="col-6 col-form-label">Antal</label>
+                <div class="col-6">
                   <input type="text" class="form-control" id="" placeholder="0">
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="col-md-3">
+          <div class="col-xs-12 col-md-6 col-lg-3">
             <div class="well shop-item">
               <img src="../images/skolporten-poster.png"/>
               <h5>Skolporten nr 1/2017</h5>
               <p>Pris: 169 Kr inklusive moms</p>
               <div class="form-group row">
-                <label for="" class="col-2 col-form-label">Antal</label>
-                <div class="col-10">
+                <label for="" class="col-6 col-form-label">Antal</label>
+                <div class="col-6">
                   <input type="text" class="form-control" id="" placeholder="0">
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="col-md-3">
+          <div class="col-xs-12 col-md-6 col-lg-3">
             <div class="well shop-item">
               <img src="../images/skolporten-poster.png"/>
               <h5>Skolporten nr 1/2017</h5>
               <p>Pris: 169 Kr inklusive moms</p>
               <div class="form-group row">
-                <label for="" class="col-2 col-form-label">Antal</label>
-                <div class="col-10">
+                <label for="" class="col-6 col-form-label">Antal</label>
+                <div class="col-6">
                   <input type="text" class="form-control" id="" placeholder="0">
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="col-md-3">
+          <div class="col-xs-12 col-md-6 col-lg-3">
             <div class="well shop-item">
               <img src="../images/skolporten-poster.png"/>
               <h5>Skolporten nr 1/2017</h5>
               <p>Pris: 169 Kr inklusive moms</p>
               <div class="form-group row">
-                <label for="" class="col-2 col-form-label">Antal</label>
-                <div class="col-10">
+                <label for="" class="col-6 col-form-label">Antal</label>
+                <div class="col-6">
                   <input type="text" class="form-control" id="" placeholder="0">
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="col-md-3">
+          <div class="col-xs-12 col-md-6 col-lg-3">
             <div class="well shop-item">
               <img src="../images/skolporten-poster.png"/>
               <h5>Skolporten nr 1/2017</h5>
               <p>Pris: 169 Kr inklusive moms</p>
               <div class="form-group row">
-                <label for="" class="col-2 col-form-label">Antal</label>
-                <div class="col-10">
+                <label for="" class="col-6 col-form-label">Antal</label>
+                <div class="col-6">
                   <input type="text" class="form-control" id="" placeholder="0">
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="col-md-3">
+          <div class="col-xs-12 col-md-6 col-lg-3">
             <div class="well shop-item">
               <img src="../images/skolporten-poster.png"/>
               <h5>Skolporten nr 1/2017</h5>
               <p>Pris: 169 Kr inklusive moms</p>
               <div class="form-group row">
-                <label for="" class="col-2 col-form-label">Antal</label>
-                <div class="col-10">
+                <label for="" class="col-6 col-form-label">Antal</label>
+                <div class="col-6">
                   <input type="text" class="form-control" id="" placeholder="0">
                 </div>
               </div>
@@ -114,28 +114,28 @@
 
           <h6 class="section-header col-md-12">Övriga böcker</h6>
 
-          <div class="col-md-3">
+          <div class="col-xs-12 col-md-6 col-lg-3">
             <div class="well shop-item">
               <img src="../images/skolporten-poster.png"/>
               <h5>Skolporten nr 1/2017</h5>
               <p>Pris: 169 Kr inklusive moms</p>
               <div class="form-group row">
-                <label for="" class="col-2 col-form-label">Antal</label>
-                <div class="col-10">
+                <label for="" class="col-6 col-form-label">Antal</label>
+                <div class="col-6">
                   <input type="text" class="form-control" id="" placeholder="0">
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="col-md-3">
+          <div class="col-xs-12 col-md-6 col-lg-3">
             <div class="well shop-item">
               <img src="../images/skolporten-poster.png"/>
               <h5>Skolporten nr 1/2017</h5>
               <p>Pris: 169 Kr inklusive moms</p>
               <div class="form-group row">
-                <label for="" class="col-2 col-form-label">Antal</label>
-                <div class="col-10">
+                <label for="" class="col-6 col-form-label">Antal</label>
+                <div class="col-6">
                   <input type="text" class="form-control" id="" placeholder="0">
                 </div>
               </div>
@@ -146,114 +146,119 @@
 
         <div class="row">
 
-          <div class="col-md-6">
+          <div class="col-xs-12 col-md-12 col-lg-6">
             <div class="well">
-            <h3>Beställningsuppgifter</h3>
+              <h3>Beställningsuppgifter</h3>
 
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Kontaktperson:*</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Kontaktperson:*</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
               </div>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">E-post:</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Telefon:*</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <h6 class="section-header">Leveransadress</h6>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Företag:*</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Refnr/adress:</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Adress:</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Postnummer:*</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Ort:</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <h6 class="section-header">Fakturaadress (om annat än ovanstående)</h6>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Företag:</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Refnr/adress:</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Adress:</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Postnummer:</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="" class="col-4 col-form-label">Ort:</label>
+                <div class="col-8">
+                  <input type="text" class="form-control" id="" placeholder="">
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-8">
+                  <p class="small">Fält markerade med * är obligatoriska
+                    <br/>Om du har problem med din beställning, kontakta john.miller[at]skolporten.se så hjälper han dig med din order.</p>
+                </div>
+
+                <div class="col-md-4">
+                  <button type="button" class="btn btn-primary btn-block">Skicka</button>
+                </div>
+
+              </div>
+
             </div>
 
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">E-post:</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Telefon:*</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <h6 class="section-header">Leveransadress</h6>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Företag:*</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Refnr/adress:</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Adress:</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Postnummer:*</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Ort:</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <h6 class="section-header">Fakturaadress (om annat än ovanstående)</h6>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Företag:</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Refnr/adress:</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Adress:</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Postnummer:</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="" class="col-4 col-form-label">Ort:</label>
-              <div class="col-8">
-                <input type="text" class="form-control" id="" placeholder="">
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="col-md-8">
-                <p class="small">Fält markerade med * är obligatoriska
-                  <br/>Om du har problem med din beställning, kontakta john.miller[at]skolporten.se så hjälper han dig med din order.</p>
-              </div>
-
-              <div class="col-md-4"><button type="button" class="btn btn-primary btn-block">Skicka</button></div>
-
-          </div>
           </div>
 
         </div>
