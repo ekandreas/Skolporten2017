c743d6b696cd69b2d26fed5425396dae56a37473
Daniel Lauding <daniel@lauding.se>
2017-05-09 15:02:05 +0200

    banner fix

---
+++ public/landningssida.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <title>Skolporten.se</title>
+  <meta content="Skolporten" name="description">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <link href="css/style.css" rel="stylesheet">
+  </head>
+<body>
+
+  <?php include("partials/header.php");?>
+
+  <?php include("partials/cover.php");?>
+
+  <div class="container wrapper">
+
+    <div class="row">
+
+      <div class="col-xs-12 col-md-12 col-lg-7 news-main">
+
+        <div class="article-short">
+
+          <div class="meta">
+            <ul class="meta-tags">
+              <li class="meta-tag"><a href="">Intervju</a></li>
+            </ul>
+            <a href="#" class="meta-link" target="_blank">2016-12-19 11:09</a>
+            <div class="dropdown d-inline">
+              <a href="#" class="meta-link dropdown-toggle" target="_blank" data-toggle="dropdown"><img src="css/gfx/icon-share.png"/> Dela</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                <li><a href="#">Dela på Twitter</a></li>
+                <li><a href="#">Dela på Facebook</a></li>
+                <li><a href="#">Dela på LinkedIn</a></li>
+              </ul>
+            </div>
+            <a href="#" class="meta-link" target="_blank"><img src="css/gfx/icon-print.png"/> Skriv ut</a>
+          </div>
+
+          <div class="article-content">
+            <h2>LO: Förstatliga skolan</h2>
+            <p class="lead">Pisa-mätningarna visar att den svenska skolan har blivit mindre likvärdig. Nu uttalar sig LO i frågan och säger att ”ett förstatligande av skolan bör övervägas”.</p>
+
+            <div class="interview widget well">
+              <h6 class="section-header hidden-xs-down">Faktaruta</h6>
+              <img src="../images/interview-image.png"/>
+
+              <dl>
+                <dt>Heléne Zetterström Dahlqvist</dt>
+                <dd></dd>
+                <dt>Född:</dt>
+                <dd>1974</dd>
+                <dt>Bor:</dt>
+                <dd>Njurunda</dd>
+                <dt>Disputerade:</dt>
+                <dd>2016-12-02 vid Mittuniversitetet</dd>
+              </dl>
+
+              <a class="btn btn-link" href="http://www.skolporten.se/forskning/intervju/multiplikation-av-flersiffriga-tal-utmanar/students-understandings-multiplication">Avhandlning</a>
+
+            </div>
+
+            <h3>Varför blev du intresserad av ämnet?</h3>
+            <p>– Jag är folkhälsovetare och har tidigare arbetat som folkhälsoplanerare inom landstinget. Där kom jag i kontakt med Disa, en metod som används i skolan för att minskar depressiva symtom hos unga. Disa påstods vara evidensbaserad men vid en närmare granskning såg jag att evidensen bestod av en magisteruppsats. När jag fick möjlighet att forska ville jag undersöka effekten av Disa men också vilka faktorer som ligger bakom depressiva symptom bland ungdomar.</p>
+
+            <h3>Vad handlar avhandlingen om?</h3>
+            <p>– Om vilken effekt Disa har på de ungdomar som erbjuds metoden. Men också vilka individuella, psykosociala och strukturella faktorer som kan ha betydelse för depressiva symptom bland unga. Avhandlingen bygger på enkäter bland drygt 1000 elever mellan 14-16 år i en kommun. Enkäterna gjordes vid tre tillfällen med ett års mellanrum. Eleverna har fått svara på frågor om självkänsla, relationer med föräldrar, kompisar, lärare, sexuella trakasserier, mobbning, invandrarbakgrund och materiellt välstånd. De har också fått svara på om de gått Disa-programmet under året.</p>
+
+            <h3>Vilka är de viktigaste resultaten?</h3>
+            <p>– Att sexuella trakasserier är en stark faktor bakom depressiva symptom bland tjejer i den här åldern. Med sexuella trakasserier menas här att mot sin vilja bli utsatt för glåpord, ryktesspridning, tafsad på, få bilder med sexuellt innehåll skickade till sig, bli offentlig kommenterad för sin egen kropp men också rena övergrepp.</p>
+            <p>– Resultaten visar att tjejer och killar i lika hög grad är utsatta för sexuella trakasserier. Men det är nästan bara är för tjejer som detta har samband med depressiva symptom. Verbala trakasserier med homofobiskt innehåll är vanligare bland killar medan tjejerna även trakasseras fysiskt eller genom offentlig kränkning. Den som trakasserar är i de allra flesta fall en kille i samma skola, därefter en kille i en annan skola, pojkvän, en kille som de inte känner och i sista hand en vuxen. Även tjejer trakasserar, tidigare forskning visar att det då ofta handlar om att ”ge igen”. De killar som utsätts för trakasserier mår ofta redan dåligt. Den viktigaste bestämningsfaktorn för depressiva symptom bland pojkar är att inte ha lika mycket pengar som kompisarna.</p>
+            <p>– Min analys är feministiskt – jag menar att skillnaderna i resultat när det gäller sexuella trakasserier mellan tjejer och killar förklaras av att sexuella trakasserier är ett verktyg för att skapa en viss maktordning där den överordnade maskuliniteten dominerar. Tjejer som blir utsatta för sexuella trakasserier förkroppsligar den värld de lever i. De killar som mår dåligt blir utsatta för att de inte är tillräckligt maskulina utifrån det idealiserade sätt att vara kille på.</p>
+            <p>– De elever som erbjudits Disa mellan enkätundersökningarna mådde varken bättre eller sämre efteråt. Insatsen gav helt enkelt inte någon effekt. Min tolkning av detta är att Disa erbjuds till de elever som mår sämst. Men Disa är en förebyggande metod, inte någon behandling. Mycket tyder på att skolorna erbjuder Disa till fel målgrupp och att det är därför som metoden inte visar på några positiva effekter.</p>
+
+            <h3>Vad överraskade dig?</h3>
+            <p>– Att sexuella trakasserier är en så stark faktor bakom depressiva symptom.</p>
+
+            <h3>Vem har nytta av dina resultat?</h3>
+            <p>– Skolorna förstås. När jag pratar med lärare om sexuella trakasserier säger många att, ”nej det är inte utbrett på vår skola”. Men när jag definierar vad jag menar med sexuella trakasserier, det vill säga verbala kränkningar, dra i underkläder och liknande, ja då erkänner de flesta att det är vanligt förekommande. Jag tror att skolorna måste prata mer om vad sexuella trakasserar faktiskt är.</p>
+
+            <button type="button" class="btn btn-link">Susanne Sawander</button>
+
+          </div>
+
+          <div class="history">
+            <p class="small">Sidan skapades 2017-01-18 av Susanne Sawander<br/>
+            Senast redigerad 2017-01-18 av Susanne Sawander</p>
+          </div>
+
+          <hr/>
+
+          <h6 class="section-header">Relaterat</div>
+
+          <div class="row">
+
+            <div class="col-md-6 article-short">
+
+              <div class="article-content">
+                <h3><a href="">Social bakgrund styr valet att bli gymnasielärare</a></h3>
+                <p class="small">Den sociala bakgrunden har stor betydelse för vem som blir gymnasielärare. Det konstaterar Magnus Persson som forskat om vad som sker under gymnasielärarutbildningen.</p>
+              </div>
+
+              <div class="meta">
+                <ul class="meta-tags">
+                  <li class="meta-tag tag-interview"><a href="">Intervju</a></li>
+                </ul>
+              </div>
+
+            </div>
+
+            <div class="col-md-6 article-short">
+
+              <div class="article-content">
+                <h3><a href="">Problematisering viktigt i religionskunskap</a></h3>
+                <p class="small">Att kunna problematisera ämnets innehåll och visa på relevanta sammanhang är viktiga förmågor hos religionskunskapslärare. David Carlsson har forskat om vilka kunskaper lärare i religionskunskap på gymnasiet anses behöva.</p>
+              </div>
+
+              <div class="meta">
+                <ul class="meta-tags">
+                  <li class="meta-tag tag-interview"><a href="">Intervju</a></li>
+                </ul>
+              </div>
+
+            </div>
+
+            <div class="col-md-6 article-short">
+
+              <div class="article-content">
+                <h3><a href="">Skolbibliotek 2017, 29-30 mars i Göteborg</a></h3>
+                <p class="small">Konferensen för dig som arbetar i och med skolbibliotek i grundskolan eller gymnasieskolan. Vi berör bl.a. ämnen som digitalisering, samverkan lärare-skolbibliotek, yttrande-och tryckfrihetslagen samt att bygga ett mångspråkigt bokbestånd. Välkommen till konferensen om skolbiblioteket - skolans nav!</p>
+              </div>
+
+              <div class="meta">
+                <ul class="meta-tags">
+                  <li class="meta-tag tag-events"><a href="">Arrangemang</a></li>
+                </ul>
+              </div>
+
+            </div>
+
+            <div class="col-md-6 article-short">
+
+              <div class="article-content">
+                <h3><a href="">Specialpedagogik vårterminen 2017</a></h3>
+                <p class="small">Intresserad av fortbildning inom det specialpedagogiska fältet? Välkommen till vår årliga konferens, den 27-28 april i Stockholm och den 4-5 maj i Göteborg. Lyssna bland annat till Petri Partanen om metakognition, Bo Sundblad om "pysparagrafen" och Alva Appelgren om samband mellan feedback och motivation. Vi skapar en röd tråd mellan lärprocesser, motivation och förhållningssätt!</p>
+              </div>
+
+              <div class="meta">
+                <ul class="meta-tags">
+                  <li class="meta-tag tag-events"><a href="">Arrangemang</a></li>
+                </ul>
+              </div>
+
+            </div>
+
+          </div>
+
+
+      </div>
+
+      <div class="col-xs-12 col-md-12 col-lg-4  offset-xs-0 offset-md-0 offset-lg-1 news-sidebar">
+
+        <?php include("partials/sidebar.php");?>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <?php include("partials/footer.php");?>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ public/partials/cover.php
@@ -0,0 +1,19 @@
+<div class="cover" style="background-image:url(../images/bg-ex.jpg);">
+
+  <div class="container wrapper">
+
+    <div class="row">
+
+      <div class="col-sm-6">
+        <div class="cover-content">
+          <h1 class="cover-title">Betyg & Bedömning</h1>
+          <p class="lead">Lorem ipsum dolor sit amet, vis no inermis copiosae, aliquid copiosae per et. Eos in dicta alienum moderatius</p>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+
+</div>
