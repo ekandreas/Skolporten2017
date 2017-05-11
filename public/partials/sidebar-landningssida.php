e1e4e5f4b583f07d597f01c0dabb8d5c0cda3d48
Daniel Lauding <daniel@lauding.se>
2017-05-11 08:47:27 +0200

    landningssida

---
+++ public/partials/sidebar-landningssida.php
@@ -0,0 +1,50 @@
+<h6 class="section-header hidden-xs-down">FORSKNING OM BETYG & BEDÖMNING</h6>
+
+<div class="article-short">
+
+  <div class="article-content">
+    <h3><a href="">Ämnesbetyg möter motstånd</a></h3>
+    <p class="small">Sveriges Kommuner och Landsting är kritiska till att införa ämnesbetyg. Även andra instanser är tveksamma till gymnasieutredningens förslag.</p>
+  </div>
+
+  <div class="meta">
+    <a href="#" class="meta-link" target="_blank">Gymnasiet <img src="css/gfx/icon-link.png"/></a>
+  </div>
+
+</div>
+
+<div class="article-short">
+
+  <div class="article-content">
+    <h3><a href="">500 Läxhjälpare stödjer elever med skagiga betyg</a></h3>
+    <p class="small">Nominerad till Årets samhällsentreprenör: Läxhjälpen. 1 500 elever med stor risk att inte klara godkända avgångsbetyg i grundskolan får individuellt studiestöd i Stiftelsen Läxhjälpens program i dag. Erfarenheten talar ­tydligt: de allra flesta kommer att lyckas.</p>
+  </div>
+
+  <div class="meta">
+    <a href="#" class="meta-link" target="_blank">Dagens samhälle <img src="css/gfx/icon-link.png"/></a>
+  </div>
+
+</div>
+
+<div class="article-short">
+
+  <div class="article-content">
+    <h3><a href="">Saknas behöriga lärare inför betygsättning</a></h3>
+    <p class="small">Snart ska högstadieeleverna få betyg. Men på flera Motalaskolor sköts undervisningen av obehöriga vikarier som inte har rätt att ensamma sätta betyg.</p>
+  </div>
+
+  <div class="meta">
+    <ul class="meta-tags">
+      <li class="meta-tag"><a href="">Lärarbrist</a></li>
+    </ul>
+    <a href="#" class="meta-link" target="_blank">Corren <img src="css/gfx/icon-link.png"/></a>
+  </div>
+
+</div>
+
+<div class="banner-area hidden-xs-down">
+  <h6 class="banner-title">ANNONS:</h6>
+  <div class="banner-content">
+    <a href=""><img src="images/wwf-banner.png"/></a></a>
+  </div>
+</div>
