64006b056ee498ebbe99f667391fe1db5c8a326d
Andreas Ek <andreas@elseif.se>
2017-05-16 21:50:45 +0200

    Teather missing to Bootstrap Moving all scripts to one php-include

--- public/news.php
+++ public/news.php
@@ -35,9 +35,7 @@
 
   <?php include("partials/footer.php");?>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/scripts.js"></script>
+  <?php include("partials/scripts.php");?>
+
 </body>
 </html>
